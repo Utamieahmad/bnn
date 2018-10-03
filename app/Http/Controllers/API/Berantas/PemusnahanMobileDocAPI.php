f4184b33140d3a16fb0ef74126ec8f21f3646c19
dendry <dendryjava@gmail.com>
2018-10-03 17:51:47 +0700

    data gw

---
+++ app/Http/Controllers/API/Berantas/PemusnahanMobileDocAPI.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @SWG\Post(
+ *   path="/pemusnahanmobile",
+ *   tags={"List Pemusnahan Mobile"},
+ *   summary="Post Pemusnahan Mobile",
+ *   operationId="getList",
+ *   @SWG\Parameter(
+ *     name="Authorization",
+ *     in="header",
+ *     description="Authorization Token",
+ *     required=true,
+ *     type="string"
+ *   ),
+ *   @SWG\Parameter(
+ *     name="page",
+ *     in="formData",
+ *     description="Pagination",
+ *     required=false,
+ *     type="string"
+ *   ),
+ *   @SWG\Response(response=200, description="successful operation"),
+ *   @SWG\Response(response=406, description="not acceptable"),
+ *   @SWG\Response(response=500, description="internal server error")
+ * )
+ *
+ */
+
+?>
