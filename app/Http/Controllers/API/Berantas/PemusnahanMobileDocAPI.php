1a7e5a3d8cb6fffa71d75b6c30c38bfaf618bc06
dendry <dendryjava@gmail.com>
2018-10-04 09:50:46 +0700

    iseng aja

--- app/Http/Controllers/API/Berantas/PemusnahanMobileDocAPI.php
+++ app/Http/Controllers/API/Berantas/PemusnahanMobileDocAPI.php
@@ -27,4 +27,31 @@
  *
  */
 
+
+/**
+ * @SWG\Post(
+ *   path="/listpemusnahanmobile",
+ *   tags={"List Pemusnahan Mobile"},
+ *   summary="Post List Pemusnahan Mobile",
+ *   operationId="postList",
+ *   @SWG\Parameter(
+ *     name="Authorization",
+ *     in="header",
+ *     description="Authorization Token",
+ *     required=true,
+ *     type="string"
+ *   ),
+ *   @SWG\Parameter(
+ *     name="page",
+ *     in="formData",
+ *     description="Pagination",
+ *     required=false,
+ *     type="string"
+ *   ),
+ *   @SWG\Response(response=200, description="successful operation"),
+ *   @SWG\Response(response=406, description="not acceptable"),
+ *   @SWG\Response(response=500, description="internal server error")
+ * )
+ *
+ */
 ?>
