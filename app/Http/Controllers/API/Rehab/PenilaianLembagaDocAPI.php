38554047d41208af35d84381978f6c92496bc16a
dendry <dendryjava@gmail.com>
2018-10-08 13:09:33 +0700

    penilaian lembaga dokumentasi beneran final

--- app/Http/Controllers/API/Rehab/PenilaianLembagaDocAPI.php
+++ app/Http/Controllers/API/Rehab/PenilaianLembagaDocAPI.php
@@ -1030,3 +1030,38 @@
     * )
     *
 */
+
+
+/**
+    * @SWG\Post(
+    *   path="/penilaian_lembaga_rehabilitasi",
+    *   tags={"Rehab Penilaian Lembaga"},
+    *   summary="Get List Data Penilaian Lembaga by id",
+    *   operationId="get data by id",
+    *   @SWG\Parameter(
+    *     name="Authorization",
+    *     in="header",
+    *     description="Authorization Token",
+    *     required=true,
+    *     type="string"
+    *   ),
+    *   @SWG\Parameter(
+    *     name="limit",
+    *     in="formData",
+    *     description="Limit",
+    *     required=false,
+    *     type="integer"
+    *   ),
+    *   @SWG\Parameter(
+    *     name="page",
+    *     in="formData",
+    *     description="Page",
+    *     required=false,
+    *     type="integer"
+    *   ),
+    *   @SWG\Response(response=200, description="successful operation"),
+    *   @SWG\Response(response=406, description="not acceptable"),
+    *   @SWG\Response(response=500, description="internal server error")
+    * )
+    *
+*/
